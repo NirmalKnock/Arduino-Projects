c9c644968792bfe23e1cc8fa44c11e6ec8d7efb7
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-06-13 10:29:02 +0530

    Add files via upload

---
+++ Serial UART/Serial UART.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.write(42);//which return in serial monitor as an ASCII values like * for 42 
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
