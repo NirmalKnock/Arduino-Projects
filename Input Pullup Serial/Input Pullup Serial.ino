23c6d49bbbad2c32b17d906ba0678937fd54be6d
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-06-22 19:55:32 +0530

    Add files via upload

---
+++ Input Pullup Serial/Input Pullup Serial.ino
@@ -0,0 +1,21 @@
+int input=13;
+int value;
+void setup() {
+  Serial.begin(9600);
+  pinMode(input,INPUT_PULLUP);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  value=digitalRead(input)
+  if(value==HIGH){
+    digitalWrite(intput,LOW);
+  }
+  else{
+    digitalWrite(input,HIGH);
+  }
+  
+
+}
