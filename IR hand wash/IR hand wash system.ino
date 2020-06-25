32d3f77eac77fdba80256ee4a31ad9d08e4568a3
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-06-25 19:57:20 +0530

    Update IR hand wash system.ino

--- IR hand wash/IR hand wash system.ino
+++ IR hand wash/IR hand wash system.ino
@@ -1,11 +1,11 @@
-int irsensor=9; // connect sensor out pin to 9th pin of arduino.
-int pump=8; // connect relay pin to 8 th pin of arduino.
+int irsensor=8; // connect sensor out pin to 9th pin of arduino.
+int pump=9; // connect relay pin to 8 th pin of arduino.
 int value;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(9,INPUT);
-  pinMode(8,OUTPUT);
+  pinMode(8,INPUT);
+  pinMode(9,OUTPUT);
 }
 
 void loop() {
