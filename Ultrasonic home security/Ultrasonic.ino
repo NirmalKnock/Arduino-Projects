524bb56f8edfdbd6b44c59754457b4f78e0ef05d
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-08-06 19:58:38 +0530

    Update Ultrasonic.ino

--- Ultrasonic home security/Ultrasonic.ino
+++ Ultrasonic home security/Ultrasonic.ino
@@ -1,7 +1,40 @@
-ultrasonic pin trigger=8;
-ultrasonic pin echo =9;
+const int trigPin = 7; // Connect 7 th pin of arduino.
+const int echoPin = 6; // Connect 6 th pin of arduino.
 
-void setup(){
+float duration, distance;
+
+
+void setup() {
+  
+  pinMode(trigPin, OUTPUT);  
+  
+  pinMode(echoPin, INPUT);
+  
+  Serial.begin(9600);
+
 }
-void loop(){
-}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(500);
+
+  // GSM Section
+  
+  if(distance<200){   //use can increase or decrease the distance in CM .
+    Serial.println("OK");
+    delay(20000);
+    Serial.println("ATD+YOUR NUMBER;");// change your number with country code .eg); +91XXXXXX
+    delay(150000);
+    Serial.println("ATH");
+    delay(1000);
+    }
+  }
